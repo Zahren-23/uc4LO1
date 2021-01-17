64598b374625eeb40f752c53fcf2356c2f7c5152
Zahren-23 <77035071+Zahren-23@users.noreply.github.com>
2021-01-17 15:47:08 +0800

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,32 @@
+<?php
+
+ $product = [
+     ['name' =>'shiny star', 'price' => 20],
+     ['name' =>'green shell', 'price' => 10],
+     ['name' =>'red shell', 'price' => 15],
+     ['name' =>'gold coins', 'price' => 5],
+     ['name' =>'lightning bolt', 'price' => 40],
+     ['name' =>'banana skin', 'price' => 2],
+ ];
+
+ while($i < count($products)){
+     echo $products[$i]['name'];
+     echo '<br />';
+     $i++;
+ }
+
+ // comparisons booleans (true or false)
+echo true;
+ //loose vs strict equal comparison
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+    <title>PHP Tutorials</title>
+</head>
+<body>
+
+</body>
+</html>
